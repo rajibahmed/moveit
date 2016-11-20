8c04665ad7acb56d973d715d4322ebb1cdbf36de
Rajib Ahmed <l.rajibahmed@gmail.com>
2016-11-20 16:47:44 +0100

    Add basic installation script

---
+++ setup.sh
@@ -0,0 +1,22 @@
+#!/bin/env sh
+
+rm -rf moveit-api/tmp/*
+
+if ! type -p node > /dev/null; then
+  echo "Please install nodejs" 
+  exit 1
+fi
+
+if ! type docker-compose > /dev/null; then
+  echo "Please install docker" 
+  exit 1
+fi
+
+cd moveit-ui && npm run build && cd ..
+
+docker-compose up -d
+
+docker-compose run api rake db:create 
+docker-compose run api rake db:migrate
+
+open http://localhost:8080
